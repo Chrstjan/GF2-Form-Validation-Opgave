04e44917f67ac5a7ea7e93010ac7731946fc9ff7
Chrstjan <ps5christian@hotmail.com>
2023-11-22 14:35:56 +0100

    Made the validation

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -8,6 +8,8 @@ const MypostNum = document.getElementById("postNumber");
 const postNumRegExp = /^[0-9]{2, }$/;
 const myEmail = document.getElementById("Email");
 
+const errorMessages = document.getElementsByClassName("errorMessage");
+
 form.addEventListener("submit", (e) => {
      e.preventDefault();
      
@@ -16,11 +18,12 @@ form.addEventListener("submit", (e) => {
     if (trimmedFName.length >= 2 ) {
         console.log("Navn er ok");
         fName.classList.add("formSucess");
+        errorMessages[0].innerHTML = "";
     }
     else {
         console.log("fornavn er ikke ok");
         fName.classList.add("formError");
-
+        errorMessages[0].innerHTML = "Fornavn skal være mindst 2 karaktere";
     }
 
     let trimmedLName = lName.value.trim();
@@ -28,20 +31,27 @@ form.addEventListener("submit", (e) => {
     if (trimmedLName.length >= 2) {
         console.log("efternavn er ok");
         lName.classList.add("formSucess");
+        errorMessages[1].innerHTML = "";
     }
     else {
         console.log("efternavn er ikke ok");
         lName.classList.add("formError");
+        errorMessages[1].innerHTML = "Efternavn skal være mindst 2 karaktere";
+
     }
 
     let trimmedAdresse = address.value.trim();
     if (trimmedAdresse.length >= 5) {
         console.log("Adresse er ok");
         address.classList.add("formSucess");
+        errorMessages[2].innerHTML = "";
+
     }
     else {
         console.log("Adresse er ikke ok");
         address.classList.add("formError");
+        errorMessages[2].innerHTML = "Adresse skal være mindst 5 karaktere";
+
     }
 
     const validatePost = (post) => {
@@ -55,10 +65,14 @@ form.addEventListener("submit", (e) => {
     if (validatePost(MypostNum.value)) {
         console.log("postnummer er ok");
         MypostNum.classList.add("formSucess");
+        errorMessages[3].innerHTML = "";
+
     }
     else {
         console.log("postnummer er ikke ok");
         MypostNum.classList.add("formError");
+        errorMessages[3].innerHTML = "Postnummer må kun være tal";
+
     }
 
     const validateEmail = (email) => {
@@ -72,10 +86,14 @@ form.addEventListener("submit", (e) => {
     if (validateEmail(myEmail.value)) {
         console.log("email er ok");
         myEmail.classList.add("formSucess");
+        errorMessages[4].innerHTML = "";
+
     }
     else {
         console.log("email er ikke ok");
         myEmail.classList.add("formError");
+        errorMessages[4].innerHTML = "Email skal indeholde et @ og et .";
+
     }
 
 
